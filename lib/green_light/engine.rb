bea132c577af7a08665c09eade02d51f43b12b89
Seth Bro <seth@sethbro.com>
2012-01-23 22:41:17 -0600

    Attempt to add javascript to assets precompile

--- lib/green_light/engine.rb
+++ lib/green_light/engine.rb
@@ -1,4 +1,12 @@
 module GreenLight #:nodoc:
   class Engine < ::Rails::Engine #:nodoc:
+  
+  # Enabling assets precompiling under rails 3.1
+    if Rails.version >= '3.1'
+      initializer :assets do |config|
+        Rails.application.config.assets.precompile += %w( vendor/green_light/validations.js )
+      end
+    end
+    
   end
 end
