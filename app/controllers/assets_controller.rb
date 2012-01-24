0359bf907dc58d6611a8f922f08c111db76c37b9
Seth Bro <sethmbro@gmail.com>
2012-01-24 11:32:24 -0600

    Remove controllers & views

--- app/controllers/assets_controller.rb
+++
@@ -1,8 +0,0 @@
-class AssetsController < ApplicationController
-  respond_to :js
-
-  def green_light
-    @rules = GreenLight::Rules.generate(GREEN_LIGHT[:validate_models])
-    respond_with(@rules, :layout => false)
-  end
-end
