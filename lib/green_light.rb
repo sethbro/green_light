3c98bce325939bd2aab7e2e5b3ddd4048d2ba93f
Seth Bro <sethmbro@gmail.com>
2012-01-26 11:14:30 -0600

    Adding config

--- lib/green_light.rb
+++ lib/green_light.rb
@@ -2,13 +2,8 @@ require 'active_support/all'
 require File.join(File.dirname(__FILE__), 'green_light/engine')
 
 module GreenLight
-  autoload :Rules,                          'green_light/rules'
-
-  module Validations
-    autoload :ValidatesFormatOf,            'green_light/validations/validates_format_of'
-    autoload :ValidatesLengthOf,            'green_light/validations/validates_length_of'
-    autoload :ValidatesNumericalityOf,      'green_light/validations/validates_numericality_of'
-    autoload :ValidatesPresenceOf,          'green_light/validations/validates_presence_of'
-    autoload :ValidatesUniquenessOf,        'green_light/validations/validates_uniqueness_of'
+  autoload :Rules, 'green_light/rules'
+  module Translations
+    autoload :JQueryValidate, 'green_light/translations/jquery_validate'
   end
 end
