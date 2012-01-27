74899d0611a1de62ab5928ebe03ff64efc468661
Seth Bro <sethmbro@gmail.com>
2012-01-27 16:42:31 -0600

    Make config vars accessible

--- lib/green_light.rb
+++ lib/green_light.rb
@@ -6,10 +6,14 @@ module GreenLight #:nodoc:
 
   class Config
     class << self
-      attr_accessor :models, :url_uniqueness_validator
+      attr_accessor :models, :url_uniqueness_validator,
+        :output_dir, :output_file, :output_var
 
       @models = []
       @url_uniqueness_validator = '/green_light/validate_unique'
+      @output_dir = 'app/assets/javascripts/green_light'
+      @output_file = 'validations.js'
+      @output_var = 'GreenLight.validations'
     end
   end
 end
